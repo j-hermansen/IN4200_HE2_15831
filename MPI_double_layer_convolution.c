2d0cc398cdab3eaeeaec1fa58983e40ddd86cddd
Jonathan Hermansen <jhermansen@live.no>
2021-05-07 04:11:32 +0200

    WIP

--- MPI_double_layer_convolution.c
+++ MPI_double_layer_convolution.c
@@ -13,7 +13,6 @@ void mat_mul(int M, int N, int K, float **input, float **kernel, float **output)
             temp = 0.0;
             for (int k=0; k < K; k++) {
                 for (int l = 0; l < K; l++) {
-                    printf("input[%d][%d]\n", i + k, j + l);
                     temp += input[i + k][j + l] * kernel[k][l];
                 }
             }
@@ -23,28 +22,20 @@ void mat_mul(int M, int N, int K, float **input, float **kernel, float **output)
 
 }
 
-void MPI_double_layer_convolution(int M, int N, float **input, int K1, float **kernel1, int K2, float **kernel2, float **output, int *sendcounts, int *Sdispls, int *n_rows, int *Gdispls, int my_rank) {
+void MPI_double_layer_convolution(int M, int N, float **input, int K1, float **kernel1, int K2, float **kernel2, float **output, int *sendcounts, int *scatter_displacements, int *n_rows, int *gather_displacements, int my_rank) {
 
-    double temp;
 
     // Scatter input to do first calculation with kernel 1
-    MPI_Scatterv(input, sendcounts, Sdispls, MPI_FLOAT, input, N*n_rows[my_rank], MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(input, sendcounts, scatter_displacements, MPI_FLOAT, input, N*n_rows[my_rank], MPI_FLOAT, 0, MPI_COMM_WORLD);
     float **output_kernel1;
     allocate_2d_array(&output_kernel1, M - K1 + 1, N - K1 + 1);
     mat_mul(n_rows[my_rank], N, K1, input, kernel1, output_kernel1);
-    MPI_Gatherv(output_kernel1, n_rows[my_rank], MPI_FLOAT, output_kernel1, n_rows, Gdispls, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(output_kernel1, n_rows[my_rank], MPI_FLOAT, output_kernel1, n_rows, gather_displacements, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    printf("OUTPUT_KERNEL1 2D ARRAY:\n");
-    print_2d_array(output_kernel1, M - K1 + 1, N - K1 + 1);
-
-    MPI_Scatterv(output_kernel1, sendcounts, Sdispls, MPI_FLOAT, output_kernel1, N*n_rows[my_rank], MPI_FLOAT, 0, MPI_COMM_WORLD);
-    mat_mul(((M - K1 + 1) - K2), ((N - K1 + 1) - K2), K2, output_kernel1, kernel2, output);
-    MPI_Gatherv(output_kernel1, n_rows[my_rank], MPI_FLOAT, output_kernel1, n_rows, Gdispls, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    if (my_rank == 0) {
-        printf("OUTPUT 2D ARRAY:\n");
-        print_2d_array(output, M - K1 - K2 + 2, N - K1 - K2 + 2);
-    }
+    // Scatter output of first kernel calculation to do the second calculation with kernel 2
+    MPI_Scatterv(output_kernel1, sendcounts, scatter_displacements, MPI_FLOAT, output_kernel1, N*n_rows[my_rank], MPI_FLOAT, 0, MPI_COMM_WORLD);
+    mat_mul((M - K1 + 1), (N - K1 + 1), K2, output_kernel1, kernel2, output);
+    MPI_Gatherv(output, n_rows[my_rank], MPI_FLOAT, output, n_rows, gather_displacements, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     free_2d_array(output_kernel1);
 
--- MPI_main.c
+++ MPI_main.c
@@ -55,73 +55,63 @@ int main(int nargs, char **args) {
             }
         }
 
-//        /// Print out 2D arrays
-//        printf("INPUT 2D ARRAY:\n");
-//        printmat(input, N, M);
-//        printf("\n");
-//        printf("KERNEL1 2D ARRAY:\n");
-//        printmat(kernel1, K1, K1);
-//        printf("\n");
-//        printf("KERNEL2 2D ARRAY:\n");
-//        printmat(kernel2, K2, K2);
-//        printf("\n");
+        // Print out 2D arrays
+        printf("INPUT 2D ARRAY:\n");
+        print_2d_array(input, N, M);
+        printf("\n");
+        printf("KERNEL1 2D ARRAY:\n");
+        print_2d_array(kernel1, K1, K1);
+        printf("\n");
+        printf("KERNEL2 2D ARRAY:\n");
+        print_2d_array(kernel2, K2, K2);
+        printf("\n");
 
     }
 
-//    MPI_Barrier(MPI_COMM_WORLD);
-
-    int *n_rows = malloc(number_of_processes * sizeof *n_rows);           // Should hold number of rows per process (a row is here the row size of K1 or K2)
-    int *sendcounts = malloc(number_of_processes * sizeof *sendcounts);   // Should hold count of types that's being scattered
-    int *scatter_displacements = malloc(number_of_processes * sizeof *scatter_displacements);         // Should
-    int *gather_displacements = malloc(number_of_processes * sizeof *gather_displacements);         // Should
-    int rows = (N / number_of_processes);                                      // Number of rows per process (some will get additional rows). It should be chunks of K1 rows.
-    int remainder = N % number_of_processes;                                   // Should hold the remainder when N is no t divisable with number of processes
+    int *n_rows = malloc(number_of_processes * sizeof *n_rows);                                // Should hold number of rows per process (a row is here the row size of K1 or K2)
+    int *sendcounts = malloc(number_of_processes * sizeof *sendcounts);                        // Should hold count of types that's being scattered
+    int *scatter_displacements = malloc(number_of_processes * sizeof *scatter_displacements);  // Should hold where every process starts reading from input
+    int *gather_displacements = malloc(number_of_processes * sizeof *gather_displacements);    // Should hold where every process should gather the data from
+    int rows = (N / number_of_processes);                                                           // Number of rows per process (some will get additional rows). It should be chunks of K1 rows.
+    int remainder = N % number_of_processes;                                                        // Should hold the remainder when N is no t divisible with number of processes
 
     scatter_displacements[0] = 0;
     gather_displacements[0] = 0;
 
-    MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);      /// Process 0 should broadcast value M to all other processes
-    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);      /// Process 0 should broadcast value N to all other processes
-    MPI_Bcast(&K1, 1, MPI_INT, 0, MPI_COMM_WORLD);     /// Process 0 should broadcast value K1 to all other processes
-    MPI_Bcast(&K2, 1, MPI_INT, 0, MPI_COMM_WORLD);     /// Process 0 should broadcast value K2 to all other processes
-
-    MPI_Bcast(&n_rows, number_of_processes, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);      // Process 0 should broadcast value M to all other processes
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);      // Process 0 should broadcast value N to all other processes
+    MPI_Bcast(&K1, 1, MPI_INT, 0, MPI_COMM_WORLD);     // Process 0 should broadcast value K1 to all other processes
+    MPI_Bcast(&K2, 1, MPI_INT, 0, MPI_COMM_WORLD);     // Process 0 should broadcast value K2 to all other processes
 
     if (my_rank > 0) {
-        allocate_2d_array(&kernel1, K1, K1);                     /// Allocated kernel 1 with K1 rows and K1 columns
-        allocate_2d_array(&kernel2, K2, K2);                     /// Allocated kernel 2 with K2 rows and K2 columns
+        allocate_2d_array(&kernel1, K1, K1);           // Allocated kernel 1 with K1 rows and K1 columns
+        allocate_2d_array(&kernel2, K2, K2);           // Allocated kernel 2 with K2 rows and K2 columns
     }
 
     for (int rank = 0; rank < number_of_processes-1; rank++) {
-        n_rows[rank] = rows + ((rank >= (number_of_processes - remainder)) ? 1:0);   /// Set number of rows for a given process
-        sendcounts[rank] = n_rows[rank]*N;                                 /// Set count of data types to send for given process
+        n_rows[rank] = rows + ((rank >= (number_of_processes - remainder)) ? 1:0);   // Set number of rows for a given process
+        sendcounts[rank] = n_rows[rank]*N;                                           // Set count of data types to send for given process
         scatter_displacements[rank+1] = scatter_displacements[rank] + (sendcounts[rank] - 2*N);
         gather_displacements[rank+1] = gather_displacements[rank] + n_rows[rank];
     }
-    n_rows[number_of_processes-1] = rows + ((number_of_processes-1) >= (number_of_processes - remainder) ? 1:0);   /// Set number of rows for last process
-    sendcounts[number_of_processes-1] = n_rows[number_of_processes-1]*N;                                 /// Set count of data types to send for last process
+    n_rows[number_of_processes-1] = rows + ((number_of_processes-1) >= (number_of_processes - remainder) ? 1:0);   // Set number of rows for last process
+    sendcounts[number_of_processes-1] = n_rows[number_of_processes-1]*N;                                           // Set count of data types to send for last process
 
+    // Broadcast scatter/gather values to all other processes
+    MPI_Bcast(n_rows, number_of_processes, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sendcounts, number_of_processes, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(scatter_displacements, number_of_processes, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(gather_displacements, number_of_processes, MPI_INT, 0, MPI_COMM_WORLD);
 
-    MPI_Bcast(&kernel1, K1*K1, MPI_FLOAT, 0, MPI_COMM_WORLD);            /// Process 0 should broadcast kernel 1 to all other processes
-    MPI_Bcast(&kernel2, K2*K2, MPI_FLOAT, 0, MPI_COMM_WORLD);            /// Process 0 should broadcast kernel 2 to all other processes
+    MPI_Bcast(kernel1, K1*K1, MPI_FLOAT, 0, MPI_COMM_WORLD);  // Process 0 should broadcast kernel 1 to all other processes
+    MPI_Bcast(kernel2, K2*K2, MPI_FLOAT, 0, MPI_COMM_WORLD);  // Process 0 should broadcast kernel 2 to all other processes
 
-
-    if (my_rank == 0) {
-        printf("PRINT OUT n_rows VALUES:\n");
-        for (int i = 0; i < number_of_processes; ++i) {
-            printf("n_rows[%d]: %d\n", i, n_rows[i]);
-        }
-    }
-
-
+    // Execute double layer convolution in parallel with scatter inside the function
     double start = MPI_Wtime();;
-
     MPI_double_layer_convolution(M, N, input, K1, kernel1, K2, kernel2, output, sendcounts, scatter_displacements, n_rows, gather_displacements, my_rank);
-
     double end = MPI_Wtime();
 
     printf("Time used: %lf\n", end - start);
-
 
 
     // Should compare parallel with sequential execution
@@ -138,6 +128,9 @@ int main(int nargs, char **args) {
         printf("SEQUENTIAL 2D ARRAY OUTPUT:\n");
         print_2d_array(output_sequential2, N-K1-K2+2, M-K1-K2+2);
 
+        printf("PARALLEL 2D ARRAY OUTPUT:\n");
+        print_2d_array(output, N-K1-K2+2, M-K1-K2+2);
+
         free_2d_array(output_sequential1);
         free_2d_array(output_sequential2);
 
@@ -153,7 +146,7 @@ int main(int nargs, char **args) {
     free(gather_displacements);
     free(sendcounts);
 
-
+    // Terminate MPI execution
     MPI_Finalize();
 
     return 0;
